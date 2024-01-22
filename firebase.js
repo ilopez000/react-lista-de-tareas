b0ec6229ae59c8b5cc6a4f0438f8c1de8d5cdec0
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2024-01-22 15:34:33 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAOr4ZGZ3bQzH-3pPLaf4J6e8-_oweEr8U",
-  authDomain: "listatareasreact-ed1bf.firebaseapp.com",
-  projectId: "listatareasreact-ed1bf",
-  storageBucket: "listatareasreact-ed1bf.appspot.com",
-  messagingSenderId: "980613520418",
-  appId: "1:980613520418:web:d3d7b383512c0962b6be7b"
+  apiKey: "Pon el tuyo",
+  authDomain: "Pon el tuyo",
+  projectId: "Pon el tuyo",
+  storageBucket: "Pon el tuyo",
+  messagingSenderId: "Pon el tuyo",
+  appId: "Pon el tuyo"
 };
 
 // Inicializa Firebase
